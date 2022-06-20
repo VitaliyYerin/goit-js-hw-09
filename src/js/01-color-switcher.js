8e334abf8e4adac6dafbd992cd88c67291ad402b
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-06-20 22:29:21 +0300

    changes color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.body;
+let intervalId;
 
+buttonStart.addEventListener('click', startInterval);
+buttonStop.addEventListener('click', stopInterval);
+
+function startInterval(){
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopInterval(){
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
