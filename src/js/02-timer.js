679aafced32967c736caf85cdcd16184c1fe1e63
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-06-23 10:17:11 +0300

    changes js file

---
+++ src/js/02-timer.js
@@ -0,0 +1,70 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import {Notify} from 'notiflix/build/notiflix-notify-aio';
+
+const input = document.querySelector("#datetime-picker");
+const btnStart = document.querySelector("[data-start]");
+const dataDays = document.querySelector("[data-days]");
+const dataHours = document.querySelector("[data-hours]");
+const dataMinutes = document.querySelector("[data-minutes]");
+const dataSeconds = document.querySelector("[data-seconds]");
+let timerId;
+
+btnStart.setAttribute("disabled", true);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (options.defaultDate > selectedDates[0]) {
+      Notify.failure("Please choose a date in the future")
+    } else {
+      Notify.success("Thank you for choosing the correct date")
+      btnStart.removeAttribute("disabled")
+      btnStart.addEventListener('click', handleTimerChange);
+    }
+  },
+};
+
+const fp = flatpickr('#datetime-picker', options);
+
+function renderTimer(number) {
+  dataDays.textContent = addNunber(number.days);
+  dataHours.textContent = addNunber(number.hours);
+  dataMinutes.textContent = addNunber(number.minutes);
+  dataSeconds.textContent = addNunber(number.seconds);
+}
+
+function addNunber (value) {
+  return  value.toString().padStart(2, '0');
+};
+
+function handleTimerChange() {
+  timerId = setInterval(() => {
+    const selectedDate = fp.selectedDates[0].getTime();
+    let delta = selectedDate - new Date();
+    let dataItem = getMs(delta);
+    if (delta < 0) {
+      clearInterval(timerId);
+      input.disabled = false;
+    } else {
+      renderTimer(dataItem);
+    }
+  }, 1000);
+  input.disabled = true;
+  btnStart.setAttribute("disabled", true);
+};
+
+function getMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return {days, hours, minutes, seconds};
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector(".form")
+const delay = document.querySelector("[name=delay]")
+const step = document.querySelector("[name=step]")
+const amount = document.querySelector("[name=amount]")
+console.log(form);
+console.log(delay);
+console.log(step);
+console.log(amount);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((reselve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        reselve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  })
+  return promise
+}
+
+form.addEventListener("submit", submitForm)
+function submitForm(e) {
+  e.preventDefault()
+
+  const timer = setTimeout(() => {
+
+    for (let position = 1; position < amount.value; position += 1) {
+      let totalStep = (Number(delay.value)) + (Number(step.value)) * position
+      createPromise(position, totalStep)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+
+  }, delay.value)
+
+
 }
