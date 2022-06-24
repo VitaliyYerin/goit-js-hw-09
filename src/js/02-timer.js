4691c97b46d6811a16031572d9f69112719e131a
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-06-24 19:31:56 +0300

    changes js file

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -39,7 +39,7 @@ function renderTimer(number) {
 
 function addNunber (value) {
   return  value.toString().padStart(2, '0');
-};
+}
 
 function handleTimerChange() {
   timerId = setInterval(() => {
